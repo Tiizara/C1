bac22129e800b1c4f863e015224c7f08432f83a0
Artem <reflection744@gmail.com>
2023-06-08 14:37:49 +0500

    Сделали приветсвие

---
+++ C#5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет Маша:)");
+}
+else 
+
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
+
